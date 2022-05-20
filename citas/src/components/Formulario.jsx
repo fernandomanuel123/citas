b3770afe8295e5c58c5132d88cd249035d1fbc12
fmurgueytio <fmurgueytio@shiftlabor.com>
2022-05-19 20:09:06 -0500

    agregar paciente al listado

--- citas/src/components/Formulario.jsx
+++ citas/src/components/Formulario.jsx
@@ -1,6 +1,56 @@
-const Formulario = () => {
+import { useEffect,useState } from "react"
+import Error from "./Error";
+
+const Formulario = ({setPacientes,pacientes}) => {
+
+    const [nombre,setNombre] = useState("");
+    const [propietario,setPropietario] = useState("");
+    const [email,setEmail] = useState("");
+    const [fecha,setFecha] = useState("");
+    const [sintomas,setSintomas] = useState("");
+    const [error,setError] = useState(false);
+
+    const generarId = () =>{
+        const random = Math.random().toString(36).substr(2);
+        const fecha = Date.now().toString(36);
+
+        return random+fecha
+    }
+
+    const handleSubmit = (e) =>{
+        e.preventDefault();
+
+        if([nombre,propietario,email,fecha,sintomas].includes(""))
+        {
+            console.log("Hay al menos un campo vacío")
+            setError(true)
+            return;
+        }
+        
+        setError(false)
+
+        const objetoPaciente = {
+            nombre,
+            propietario,
+            email,
+            fecha,
+            sintomas,
+            id: generarId()
+        }
+
+        setPacientes([...pacientes,objetoPaciente])
+
+        setNombre("")
+        setPropietario("")
+        setEmail("")
+        setFecha("")
+        setSintomas("")
+
+
+    }
+
     return(
-        <div className="md:w-1/2 lg:w-2/5">
+        <div className="md:w-1/2 lg:w-2/5 mx-5">
             <h2 className="font-black text-3xl text-center">Seguimiento pacientes</h2>
         
                 <p className="text-lg mt-5 text-center mb-10">
@@ -8,7 +58,11 @@ const Formulario = () => {
                     <span className="text-indigo-600 font-bold">Administralos</span>
                 </p>
 
-                <form className="bg-white shadow-md rounded-lg py-10 px-5 mb-10">
+                <form className="bg-white shadow-md rounded-lg py-10 px-5 mb-10"
+                    onSubmit={handleSubmit}>
+
+                    {error && <Error mensaje ="Todos los campos son necesarios"/> }
+
                     <div className="mb-5">
                         <label htmlFor="mascota"
                             className="block text-gray-700 uppercase font-bold">
@@ -18,7 +72,10 @@ const Formulario = () => {
                         <input id= "mascota"
                             className="border-2 w-full p-2 mt-2 placeholder-gray-400 rounded-md"
                             type = "text"
-                            placeholder="Nombre de la mascota"/>
+                            placeholder="Nombre de la mascota"
+                            value={nombre}
+                            onChange={ (e) => setNombre(e.target.value)}
+                            />
                     </div>
 
                     <div className="mb-5">
@@ -30,7 +87,9 @@ const Formulario = () => {
                         <input id= "propietario"                            
                             className="border-2 w-full p-2 mt-2 placeholder-gray-400 rounded-md"
                             type = "text"
-                            placeholder="Nombre del propietario"/>
+                            placeholder="Nombre del propietario"
+                            value={propietario}
+                            onChange={ (e) => setPropietario(e.target.value)}/>
                     </div>
 
                     <div className="mb-5">
@@ -42,30 +101,36 @@ const Formulario = () => {
                         <input id= "email"
                             className="border-2 w-full p-2 mt-2 placeholder-gray-400 rounded-md"
                             type = "email"
-                            placeholder="Email"/>
+                            placeholder="Email"
+                            value={email}
+                            onChange={ (e) => setEmail(e.target.value)}/>
                     </div>
 
                     <div className="mb-5">
                         <label htmlFor="alta"
                             className="block text-gray-700 uppercase font-bold">
-                            Nombre del propietario
+                            Fecha
                         </label> 
                         
                         <input id= "alta"                            
                             className="border-2 w-full p-2 mt-2 placeholder-gray-400 rounded-md"
                             type = "date"
+                            value={fecha}
+                            onChange={ (e) => setFecha(e.target.value)}
                         />
                     </div>
 
                     <div className="mb-5">
                         <label htmlFor="propietario"
                             className="block text-gray-700 uppercase font-bold">
-                            Nombre del propietario
+                            Sintomas
                         </label> 
                         
                         <textarea id= "sintomas"                            
                             className="border-2 w-full p-2 mt-2 placeholder-gray-400 rounded-md"
                             placeholder="Describe los síntomas"
+                            value={sintomas}
+                            onChange={ (e) => setSintomas(e.target.value)}
                         />
                     </div>
 
--- citas/src/components/ListadoPacientes.jsx
+++ citas/src/components/ListadoPacientes.jsx
@@ -1,7 +1,43 @@
-const ListadoPacientes = () =>{
+import Paciente from "./Paciente"
+
+const ListadoPacientes = ({pacientes,setPaciente}) =>{
+
+
+
     return(
-        <div className="w-1/2 lg:w-3/5">
-            <h2>Listado pacientes</h2>
+        <div className="md:w-1/2 lg:w-3/5 md:h-screen overflow-y-scroll">
+
+            {pacientes && pacientes.length ? (
+            <>
+                <h2 className="font-black text-3xl text-center">Listado pacientes</h2>
+                <p className=" text-xl mt-5 mb-10 text-center">
+                    Administra tus {" "}
+                    <span className="text-indigo-600 font-bold"> Pacientes y citas</span>
+                </p>
+
+                {pacientes.map(paciente => {
+
+                    return(<Paciente
+                        key = {paciente.id}
+                        paciente = {paciente}
+                        setPaciente = {setPaciente}/>)
+
+                })}
+            </>
+            )
+            : (
+                <>
+                <h2 className="font-black text-3xl text-center">No hay pacientes</h2>
+                <p className=" text-xl mt-5 mb-10 text-center">
+                    Comienza agregando pacientes {" "}
+                    <span className="text-indigo-600 font-bold"> y aparecerán en este lugar</span>
+                </p>
+                </>
+            )}
+
+                  
+
+            
         </div>        
     )
 }
