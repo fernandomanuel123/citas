856e9358c7f70ede61b6f16b919422126dce6fb6
fmurgueytio <fmurgueytio@shiftlabor.com>
2022-05-20 00:32:59 -0500

    completado

--- citas/src/App.jsx
+++ citas/src/App.jsx
@@ -1,17 +1,21 @@
-import { useState } from "react"
+import { useState,useEffect } from "react"
 import Header from "./components/Header"
 import Formulario from "./components/Formulario"
 import ListadoPacientes from "./components/ListadoPacientes"
 
 function App() {
 
-  const [pacientes, setPacientes] = useState([]);
+  const [pacientes, setPacientes] = useState(JSON.parse(localStorage.getItem('pacientes')) ?? []);
   const [paciente, setPaciente] = useState({});
 
   const eliminarPaciente = (id)=>{
     const PacientesActualizados = pacientes.filter(pacienteEliminar => pacienteEliminar.id !== id)
     setPacientes(PacientesActualizados)
   }
+
+  useEffect(()=>{
+    localStorage.setItem('pacientes',JSON.stringify(pacientes))
+  },[pacientes]);
 
   return (
     <div className="container mx-auto mt-20">
